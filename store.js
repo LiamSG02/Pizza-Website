5c551ab651a085a21c308d02ac0c0345b3927329
LazyWizardMelon <89508728+LazyWizardMelon@users.noreply.github.com>
2021-09-01 13:59:36 +0200

    dont worry

--- store.js
+++ store.js
@@ -1,2 +1,26 @@
-var removeCartItemButtons = document.getElementByclassName("btn-danger) 
+var removeCartItemButtons = document.getElementsByClassName("btn-danger") 
 console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener("click", function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+    })    
+    
+}
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName("cart-item")[0]
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    var total = 0
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var cartRow = cartRow[i]
+        var priceElement = cartRow.getElementsByClassName("cart-price")[0]
+        var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+        var price = parseFloat(priceElement.innerText.replace("$", ""))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+        document.getElementsByClassName("cart-total-price")[i].innerText = "$" + total
+
+    }
+}
