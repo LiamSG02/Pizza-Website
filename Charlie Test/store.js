cb35f06e6d04dc62d0bf89616e4ff805d7740c64
LazyWizardMelon <89508728+LazyWizardMelon@users.noreply.github.com>
2021-09-02 09:19:33 +0200

    Ändrat i mina filer
    Och lagt till en folder för mina filer

--- Charlie Test/store.js
+++ Charlie Test/store.js
@@ -24,7 +24,7 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
 
 function quantityChange(event){
     var input = event.target
-    if (isNaN(input.value) input.value <= 0){
+    if (isNaN(input.value) && input.value <= 0){
         input.value = 1
     }
     updateCartTotal()
