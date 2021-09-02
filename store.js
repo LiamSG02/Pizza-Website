b3569c1dd32405a9022479b6ec8be3effdd6f410
LazyWizardMelon <89508728+LazyWizardMelon@users.noreply.github.com>
2021-09-02 08:59:59 +0200

    Update store.js

--- store.js
+++ store.js
@@ -1,4 +1,11 @@
-var removeCartItemButtons = document.getElementsByClassName("btn-danger") 
+if (document.readyStateState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+} else {
+    ready()
+}
+
+function ready(){
+    var removeCartItemButtons = document.getElementsByClassName("btn-danger") 
 console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
@@ -7,6 +14,20 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
         buttonClicked.parentElement.parentElement.remove()
     })    
     
+}
+    var quantityInputs = document.getElementsByClassName("cart-quantity-input")
+    for (var i = 0; i < quantityInput.length; i++) {
+        var input = quantityInput[i]
+        input.addEventListener("change", quantityChange)
+    }
+}
+
+function quantityChange(event){
+    var input = event.target
+    if (isNaN(input.value) input.value <= 0){
+        input.value = 1
+    }
+    updateCartTotal()
 }
 
 function updateCartTotal(){
