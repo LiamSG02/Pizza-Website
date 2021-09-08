bc1fdd87547e9f59373a1cc32c36f1082e1a5ac0
LazyWizardMelon <89508728+LazyWizardMelon@users.noreply.github.com>
2021-09-08 13:24:18 +0200

    komentarer på store.js
    eeeee

--- Charlie Test/store.js
+++ Charlie Test/store.js
@@ -1,47 +1,103 @@
-if (document.readyStateState == "loading") {
-    document.addEventListener("DOMContentLoaded", ready)
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
 } else {
     ready()
 }
-
-function ready(){
-    var removeCartItemButtons = document.getElementsByClassName("btn-danger") 
-console.log(removeCartItemButtons)
-for (var i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i]
-    button.addEventListener("click", function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-    })    
-    
-}
-    var quantityInputs = document.getElementsByClassName("cart-quantity-input")
-    for (var i = 0; i < quantityInput.length; i++) {
-        var input = quantityInput[i]
-        input.addEventListener("change", quantityChange)
+// första delen är för att remova objektet som inns i carten.
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
     }
-}
+// andra delen är hur många varor ökades det med eller hur många minskade det med.
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+// tedje delen är comandot för att adda någor till carten.
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 
-function quantityChange(event){
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+// denna function aktiveras av att du köper något i carten.
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+//  denna fuction är vad som tar bort saker från själva carten. 
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+// denna visar hur många av något du har i din cart/det är functionen som ökar/minskar hur många saker du vill ha av något.
+function quantityChanged(event) {
     var input = event.target
-    if (isNaN(input.value) && input.value <= 0){
+    if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
     updateCartTotal()
 }
-
-function updateCartTotal(){
-    var cartItemContainer = document.getElementsByClassName("cart-item")[0]
-    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+// Functionen kallar på objectets klass och allt deras klass innehåller.
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+// Denna functionen kallar på klassen av dem olika objekten och deras priser samtidigt som den uppdaterar allt som läggs tills/tars bort.
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+// Functionen uppdaterar allt så allt är rätt. Den updaterar allt som händer på sidan när något händer.
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var cartRow = cartRow[i]
-        var priceElement = cartRow.getElementsByClassName("cart-price")[0]
-        var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
-        var price = parseFloat(priceElement.innerText.replace("$", ""))
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
-        document.getElementsByClassName("cart-total-price")[i].innerText = "$" + total
-
     }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
